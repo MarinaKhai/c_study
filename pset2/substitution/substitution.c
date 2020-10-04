602bd6e434f22c15ace2178a476b3e9ff1826b2c
MarishaKhai <marinpostbox@gmail.com>
2020-10-04 23:10:55 +0500

    corrected substitution.c

--- pset2/substitution/substitution.c
+++ pset2/substitution/substitution.c
@@ -5,10 +5,16 @@
 
 int main(int argc, string argv[])
 {
-    //in case of proper number of arguments
     string key = argv[1];
     int repeated = 0;
-    if (argc == 2)
+    //if number of arguments is invalid
+    if (argc != 2)
+    {
+        printf("Usage: ./substitution key\n");
+        return 1;
+    }
+    //in case of proper number of arguments
+    else
     {
         if (strlen(key) != 26)
         {
@@ -17,6 +23,13 @@ int main(int argc, string argv[])
         }
         for (int i = 0; key[i] != '\0'; i++)
         {
+            //check if there are any non-alpha char-s
+            if (!(isalpha(key[i])))
+            {
+                printf("The key should only contain alphabetical characters\n");
+                return 1;
+            }
+            //check if a letter occures more than ones
             repeated = 0;
             for (int letter = 0; key[letter] != '\0'; letter++)
             {
@@ -30,12 +43,6 @@ int main(int argc, string argv[])
                     return 1;
                 }
             }
-
-            if (!(isalpha(key[i])))
-            {
-                printf("The key should only contain alphabetical characters\n");
-                return 1;
-            }
         }
         //make all letters in key lowercase
         for (int i = 0, n = strlen(key); key[i] != 0; i++)
@@ -46,28 +53,25 @@ int main(int argc, string argv[])
             }
         }
     }
-    //if number of arguments is invalid
-    else
-    {
-        printf("Usage: ./substitution key\n");
-    }
     string p = get_string("plaintext: ");
     string cipher = p;
     printf("ciphertext: ");
     //for alphabetical index
     int ai = 0;
+    //make substitution from plain text to cipher text
     for (int i = 0, n = strlen(cipher); i < n; i++)
     {
         if (isupper(p[i]))
         {
             ai = p[i] - 65;
-            cipher[i] = key[ai] - 32;
+            cipher[i] = key[ai] - 32; //transphorm key's lowercase letters to uppercase ones in ciphertext
         }
         else if (islower(p[i]))
         {
             ai = p[i] - 97;
             cipher[i] = key[ai];
         }
+        //for any non-alphabetical characters
         else
         {
             cipher[i] = p[i];
